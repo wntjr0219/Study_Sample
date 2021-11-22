f5e184a94f8d6d073a26fa2b8aad0e96d5ab6911
wntjr0219 <90208937+wntjr0219@users.noreply.github.com>
2021-11-23 02:48:01 +0900

    1

---
+++ map/grass.py
@@ -0,0 +1,12 @@
+from pico2d import *
+
+class World:
+    def __init__(self):
+        self.image = load_image('mario_world_1.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(1600, 600)
+  
