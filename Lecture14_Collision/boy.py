4179c61ccb18fd21081573b9e3f241e616720025
wntjr0219 <90208937+wntjr0219@users.noreply.github.com>
2021-11-23 02:47:24 +0900

    1

--- Lecture14_Collision/boy.py
+++ Lecture14_Collision/boy.py
@@ -5,15 +5,13 @@ from ball import Ball
 import game_world
 
 # Boy Run Speed
-# fill expressions correctly
-PIXEL_PER_METER = (10.0 / 0.3)
-RUN_SPEED_KMPH = 20.0
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 20.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
-RUN_SPEED_PPS = (RUN_SPEED_MPS* PIXEL_PER_METER)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 # Boy Action Speed
-# fill expressions correctly
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
@@ -68,23 +66,23 @@ class IdleState:
 class RunState:
 
     def enter(boy, event):
-       if event == RIGHT_DOWN:
-           boy.velocity += RUN_SPEED_PPS
-       elif event == LEFT_DOWN:
-           boy.velocity -= RUN_SPEED_PPS
-       elif event == RIGHT_UP:
-           boy.velocity -= RUN_SPEED_PPS
-       elif event == LEFT_UP:
-           boy.velocity += RUN_SPEED_PPS
-       boy.dir = clamp(-1, boy.velocity, 1)
-           
+        if event == RIGHT_DOWN:
+            boy.velocity += RUN_SPEED_PPS
+        elif event == LEFT_DOWN:
+            boy.velocity -= RUN_SPEED_PPS
+        elif event == RIGHT_UP:
+            boy.velocity -= RUN_SPEED_PPS
+        elif event == LEFT_UP:
+            boy.velocity += RUN_SPEED_PPS
+        boy.dir = clamp(-1, boy.velocity, 1)
+
     def exit(boy, event):
         if event == SPACE:
             boy.fire_ball()
 
     def do(boy):
-        # fill here
-        boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8 # 실수 % 8 
+        #boy.frame = (boy.frame + 1) % 8
+        boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
         boy.x += boy.velocity * game_framework.frame_time
         boy.x = clamp(25, boy.x, 1600 - 25)
 
@@ -130,7 +128,6 @@ class Boy:
         # Boy is only once created, so instance image loading is fine
         self.image = load_image('animation_sheet.png')
         self.font = load_font('ENCR10B.TTF', 16)
-
         self.dir = 1
         self.velocity = 0
         self.frame = 0
@@ -138,9 +135,13 @@ class Boy:
         self.cur_state = IdleState
         self.cur_state.enter(self, None)
 
+    def get_bb(self):
+        # fill here
+        return 0, 0, 0, 0
+
 
     def fire_ball(self):
-        ball = Ball(self.x, self.y, self. dir)
+        ball = Ball(self.x, self.y, self.dir * RUN_SPEED_PPS * 10)
         game_world.add_object(ball, 1)
 
 
@@ -157,7 +158,9 @@ class Boy:
 
     def draw(self):
         self.cur_state.draw(self)
-        self.font.draw(self.x - 60, self.y + 50, '(Time: %3.2f)' %get_time(), (255, 255, 0) )
+        self.font.draw(self.x - 60, self.y + 50, '(Time: %3.2f)' % get_time(), (255, 255, 0))
+        #fill here
+
 
     def handle_event(self, event):
         if (event.type, event.key) in key_event_table:
